e6fa90d4dcc1e9716541ec3945d21af352069179
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2023-07-22 20:43:41 +0900

    refactor

--- main.py
+++ main.py
@@ -14,14 +14,53 @@ IR_SEND_PIN_NO = board.G1
 IR_RECEIVE_PIN_NO = board.G0
 LED_PIN_NO = board.NEOPIXEL
 
-led_pin = digitalio.DigitalInOut(board.NEOPIXEL)
-led_pin.direction = digitalio.Direction.OUTPUT
+def init():
+    global led_pin, pulsein
+    print("initializing...")
+    led_pin = digitalio.DigitalInOut(board.NEOPIXEL)
+    led_pin.direction = digitalio.Direction.OUTPUT
 
-pulsein = pulseio.PulseIn(IR_RECEIVE_PIN_NO, maxlen=120, idle_state=True)
-pulsein.pause()
+    pulsein = pulseio.PulseIn(IR_RECEIVE_PIN_NO, maxlen=120, idle_state=True)
+    pulsein.pause()
+    print("done")
 
 def flash_led(g: int, r: int, b: int):
     neopixel_write.neopixel_write(led_pin, bytearray([g, r, b]))
+
+def send_ir(pulse: list[int]):
+    with pulseio.PulseOut(IR_SEND_PIN_NO) as pulseout:
+        data = array.array("H", pulse)
+
+        print("send")
+        for _ in range(3):
+            pulseout.send(array.array("H", data))
+            time.sleep(0.025)
+        print("send done")
+
+
+def receive_ir() -> list[int]:
+    timeout = 10
+
+    pulses = []
+
+    start = time.monotonic()
+    is_received = False
+    pulsein.clear()
+    pulsein.resume()
+    while not is_received and (time.monotonic() - start) < timeout:
+        time.sleep(0.1)
+        while pulsein:
+            pulse = pulsein.popleft()
+            pulses.append(pulse)
+            is_received = True
+    pulsein.pause()
+
+    if pulses is None:
+        print("cannot receive pulses")
+    else:
+        print("Heard", len(pulses), "Pulses:", pulses)
+
+    return pulses
 
 
 def run_server():
@@ -65,43 +104,14 @@ def run_server():
 
     flash_led(0, 0, 255)
 
+    print("start server")
+
     server.serve_forever(str(wifi.radio.ipv4_address))
 
 
-def send_ir(pulse: list[int]):
-    with pulseio.PulseOut(IR_SEND_PIN_NO) as pulseout:
-        data = array.array("H", pulse)
+def main():
+    init()
+    run_server()
 
-        print("send")
-        for _ in range(3):
-            pulseout.send(array.array("H", data))
-            time.sleep(0.025)
-        print("send done")
-
-
-def receive_ir() -> list[int]:
-    timeout = 10
-
-    pulses = []
-
-    start = time.monotonic()
-    is_received = False
-    pulsein.clear()
-    pulsein.resume()
-    while not is_received and (time.monotonic() - start) < timeout:
-        time.sleep(0.1)
-        while pulsein:
-            pulse = pulsein.popleft()
-            pulses.append(pulse)
-            is_received = True
-    pulsein.pause()
-
-    if pulses is None:
-        print("cannot receive pulses")
-    else:
-        print("Heard", len(pulses), "Pulses:", pulses)
-
-    return pulses
-
-
-run_server()
+if __name__ == "__main__":
+    main()
